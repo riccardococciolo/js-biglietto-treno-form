34e46f8b3488a8581ee9d5327c9da2961e106c44
riccardococciolo <coccioloriccardo64@gmail.com>
2023-10-19 17:06:58 +0200

    add: logic

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
+const userDistanceElem = document.getElementById("distance");
+const userNameElem = document.getElementById("name");
+const userAgeElem = document.getElementById("age");
+
+const sendBtn = document.getElementById("send")
+sendBtn.addEventListener("click", function () {
+    // data
+    const userDistance = userDistanceElem.value;
+    const userName = userNameElem.value;
+    const userAge = userAgeElem.value;
+    console.log(userDistance, userAge, userName);
+
+    const price = (userDistance * 0.21);
+    console.log(price);
+
+    let finalPrice = "";
+
+    if (userAge < 18) {
+        finalPrice = (price - price * 0.20);
+    } else if (userAge >= 65) {
+        finalPrice = (price - price * 0.40);
+    } else {
+        finalPrice = price;
+    }
+
+    console.log(finalPrice);
+
+    const message = `Il suo biglietto costa ${finalPrice.toFixed(2)}£ le auguriamo un buon viaggio.`;
+    console.log(message);
+});
 // data
-const distance = prompt("Quanti km dovrai percorrere?");
-
-const age = prompt("Quanti anni hai?");
-console.log(distance, age)
-
 // logic
-const price = (distance * 0.21);
-console.log(price);
 
-let finalPrice = "";
-
-if (age < 18) {
-    finalPrice = (price - price * 0.20);
-} else if (age >= 65) {
-    finalPrice = (price - price * 0.40);
-} else {
-    finalPrice = price;
-}
-
-console.log(finalPrice);
-
-const message = `Il suo biglietto costa ${finalPrice.toFixed(2)}£ le auguriamo un buon viaggio.`;
-console.log(message);
 
 // output
 document.getElementById("price").innerHTML = message;
